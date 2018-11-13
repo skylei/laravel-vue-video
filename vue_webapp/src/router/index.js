2d937d52597f965b615bc6b05fa08ba28529de1c
jiaobantang <847280718@qq.com>
2018-11-13 22:33:17 +0800

    vue页面样式显示完毕

--- vue_webapp/src/router/index.js
+++ vue_webapp/src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Video from '@/components/Video'
+import Page from '@/components/Page'
+
+
 
 Vue.use(Router)
 
@@ -8,8 +13,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/video',
+      name: 'Video',
+      component: Video
+    },
+    {
+      path: '/page',
+      name: 'Page',
+      component: Page
     }
   ]
 })
